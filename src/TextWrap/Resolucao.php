9c59143c0d2645aea1136f69ff7d03a3f8905de4
DiegoRoisCosta <69256599+DiegoRoisCosta@users.noreply.github.com>
2020-11-09 10:55:41 -0300

    Add files via upload
    Iniciar pelo index, fiz uma interface básica para exibir os dados.

--- src/TextWrap/Resolucao.php
+++ src/TextWrap/Resolucao.php
@@ -1,67 +1,126 @@
-<?php
+<!DOCTYPE html>
+<html lang="zxx">
+<head>
+     <link rel="stylesheet" href="css/style.css" type="text/css"/>
+</head>
+<body>
+   <div class="quebra-box">
+    <?php
+      
+      /**
+       * Verifica se esta vazio as entradas feitas pelo metodo POST do formulario e armazena.
+       */
+      if (!empty($_POST["texto"] && $_POST["comprimento"]))
+      {
 
-namespace Galoa\ExerciciosPhp\TextWrap;
+          $text = $_POST['texto'];
+          $length = $_POST['comprimento'];
 
-/**
- * Implemente sua resolução nessa classe.
- *
- * Depois disso:
- * - Crie um PR no github com seu código
- * - Veja o resultado da correção automática do seu código
- * - Commit até os testes passarem
- * - Passou tudo, melhore a cobertura dos testes
- * - Ficou satisfeito, envie seu exercício para a gente! <3
- *
- * Boa sorte :D
- */
-class Resolucao implements TextWrapInterface {
+      }
+      else
+      {
 
-  /**
-   * {@inheritdoc}
-   *
-   * Apague o conteúdo do método abaixo e escreva sua própria implementação,
-   * nós colocamos esse mock para poder rodar a análise de cobertura dos
-   * testes unitários.
-   */
-  public function textWrap(string $text, int $length): array {
-    if ($length === 8) {
-      return [
-        'Se vi',
-        'mais',
-        'longe',
-        'foi por',
-        'estar de',
-        'pé sobre',
-        'ombros',
-        'de',
-        'gigantes',
-      ];
+          echo "preencha os campos";
+
+      }
+
+    /**
+     * Instancia Resolucao como novo objeto.
+     */
+    $resolucao = new Resolucao($text, $length);
+
+    $textResult = $resolucao->divisaoStr();
+
+    print ($textResult);
+
+    class Resolucao
+    {
+        /**
+         * Declarando variaveis.
+         */
+        public $text;
+        public $length;
+        public $wholeText;
+        public $charTotal;
+        public $textResult;
+
+         /**
+         * O método construtor definindo os valores iniciais dos atributos do objeto.
+         */
+        public function __construct(string $text, int $length)
+        {
+
+            $this->text = $text;
+            $this->length = $length;
+
+        }
+        /**
+         * Funcao criada para realizar o processamento do texto.
+         */
+        public function divisaoStr()
+        {
+             /**
+              * Exibe o limite estipulado pelo usuario de caracateres dentro de uma linha.
+              */
+            print ("Limite por linha: " . $this->length . " caracteres<br><br>");
+
+            /**
+             * Divide o texto em elementos formados por palavras estipuladas pelo delimitador na variavel WholeText.
+             */
+            $this->wholeText = explode(" ", $this->text);
+
+            /**
+             * Usa um contador para alcançar o numero de palavras.
+             */
+            for ($i = 0;$i < count($this->wholeText);$i++)
+            {
+                /**
+                 * transfere elementos inseridos em cada indice de wholeText.
+                 */
+                $palavra = $this->wholeText[$i];
+
+                /**
+                 * para saber o limite necessario para quebra de linha é somado os caracteres de cada palavra e inserido * em charTotal.
+                 */
+                $this->charTotal = $this->charTotal + strlen($palavra);
+
+                /**
+                 * Compara charTotal com o limite estipulado e retorna quebra de linha somente quando o valor for maior,
+                 * exibindo o respectivo texto.
+                 */
+                if ($this->charTotal + 1 <= $this->length)
+                {
+
+                    if ($i == 0)
+                    {
+
+                        $this->textResult = $this->textResult . $palavra;
+
+                    }
+                    else
+                    {
+
+                        $this->textResult = $this->textResult . " " . $palavra;
+
+                    }
+
+                }
+                elseif ($this->charTotal + 1 > $this->length)
+                {
+
+                    $this->charTotal = 0;
+
+                    $this->textResult = $this->textResult . "<br>" . $palavra . " ";
+                }
+
+            }
+
+            return $this->textResult;
+        }
+
     }
-    elseif ($length === 12) {
-      return [
-        'Se vi mais',
-        'longe foi',
-        'por estar de',
-        'pé sobre',
-        'ombros de',
-        'gigantes',
-      ];
-    }
-    elseif ($length === 10) {
-      // Por favor, não implemente o código desse jeito, isso é só um mock.
-      $ret = [
-        'Se vi mais',
-        'longe foi',
-        'por estar',
-        'de pé',
-        'sobre',
-      ];
-      $ret[] = 'ombros de';
-      $ret[] = 'gigantes';
-      return $ret;
-    }
-
-    return [""];
-  }
-
-}
+    ?>
+        <br><br>
+        <a href="index.php">Voltar</a>
+    </div>
+</body>
---
+++ src/TextWrap/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="zxx">
+<head>
+	 <link rel="stylesheet" href="css/style.css" type="text/css"/>
+</head>
+<body>
+   <div class="quebra-box">
+		<h2>Quebra linha</h2>
+		<form  action="Resolucao.php" method="post">
+    		<div class="user-box">
+      			<input type="text" name="texto" name="" required="">
+      			<label>Texto</label>
+    		</div>
+    		<div class="user-box">
+      			<input type="text" name="comprimento" name="" required="">
+      			<label>Comprimento</label>
+    		</div>
+    		<input type="submit" name="submit" value="Enviar!">
+		    <span></span>
+		    <span></span>
+		    <span></span>
+		    <span></span>
+
+  		</form>
+	</div>
+</body>
